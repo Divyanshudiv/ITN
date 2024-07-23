f09689a082e0cb63b67a195b95beca15a74b0eca
Divyanshudiv <divyanshu_div@outlook.com>
2024-07-23 09:39:32 +0530

    V1

--- wfst.py
+++ wfst.py
@@ -33,27 +33,25 @@ class WFST:
     def process(self, input_sequence): 
         current_output = ''
         current_weight = 0
-        i = 0
         next_states = []
-        for j in range(len(self.states) - 1):
-            if i < len(input_sequence):
-                symbol = input_sequence[i]
-            else:
-                symbol = None
-            if symbol and symbol in self.states[j]:
-                for (next_state, output_symbol, weight) in self.states[j][symbol]:
-                    next_states.append((next_state, current_output + output_symbol, current_weight + weight))
-                    current_output += output_symbol
-                    current_weight += weight
-                    i += 1
-                    break
-            elif '' in self.states[j]:
-                for (next_state, output_symbol, weight) in self.states[j]['']:
-                    next_states.append((next_state, current_output + output_symbol, current_weight + weight))
-                    current_output += output_symbol
-                    current_weight += weight
-                    break
-            print(next_states)
+        for state in self.states:
+                values = list(self.states[state].values())
+                print('values:', values)
+                next_state = 0
+                output_symbol = ''
+                weight = 10**10
+                for itr in values:
+                    print(itr)
+                    for (buffer_next_state, buffer_output_symbol, buffer_weight) in itr:
+                        if weight >= buffer_weight:
+                            next_state = buffer_next_state
+                            output_symbol = buffer_output_symbol
+                            weight = buffer_weight
+                next_states.append((next_state, current_output + output_symbol, current_weight + weight))
+                current_output += output_symbol
+                current_weight += weight
+
+        print(next_states)
         return next_states[len(next_states) - 1]
 
     def compose(self, other, input):
@@ -155,12 +153,116 @@ hundreds_wfst.add_final_state(2)
 hundreds_wfst.add_transition(0, 1, 'hundred', '0', 0.1)
 hundreds_wfst.add_epsilon_transition(1, 2, '0', 0.1)
 
+# Thousands WFST
+thousands_wfst = WFST('thousands')
+thousands_wfst.set_start_state(0)
+thousands_wfst.add_final_state(3)
+thousands_wfst.add_transition(0, 1, 'thousand', '0', 0.1)
+thousands_wfst.add_epsilon_transition(1, 2, '0', 0.1)
+thousands_wfst.add_epsilon_transition(2, 3, '0', 0.1)
+
+# Millions WFST
+millions_wfst = WFST('millions')
+millions_wfst.set_start_state(0)
+millions_wfst.add_final_state(6)
+millions_wfst.add_transition(0, 1, 'million', '0', 0.1)
+millions_wfst.add_epsilon_transition(1, 2, '0', 0.1)
+millions_wfst.add_epsilon_transition(2, 3, '0', 0.1)
+millions_wfst.add_epsilon_transition(3, 4, '0', 0.1)
+millions_wfst.add_epsilon_transition(4, 5, '0', 0.1)
+millions_wfst.add_epsilon_transition(5, 6, '0', 0.1)
+
+# Billions WFST
+billions_wfst = WFST('billions')
+billions_wfst.set_start_state(0)
+billions_wfst.add_final_state(9)
+billions_wfst.add_transition(0, 1, 'billion', '0', 0.1)
+billions_wfst.add_epsilon_transition(1, 2, '0', 0.1)
+billions_wfst.add_epsilon_transition(2, 3, '0', 0.1)
+billions_wfst.add_epsilon_transition(3, 4, '0', 0.1)
+billions_wfst.add_epsilon_transition(4, 5, '0', 0.1)
+billions_wfst.add_epsilon_transition(5, 6, '0', 0.1)
+billions_wfst.add_epsilon_transition(6, 7, '0', 0.1)
+billions_wfst.add_epsilon_transition(7, 8, '0', 0.1)
+billions_wfst.add_epsilon_transition(8, 9, '0', 0.1)
+
+# Trillions WFST
+trillions_wfst = WFST('trillions')
+trillions_wfst.set_start_state(0)
+trillions_wfst.add_final_state(12)
+trillions_wfst.add_transition(0, 1, 'trillion', '0', 0.1)
+trillions_wfst.add_epsilon_transition(1, 2, '0', 0.1)
+trillions_wfst.add_epsilon_transition(2, 3, '0', 0.1)
+trillions_wfst.add_epsilon_transition(3, 4, '0', 0.1)
+trillions_wfst.add_epsilon_transition(4, 5, '0', 0.1)
+trillions_wfst.add_epsilon_transition(5, 6, '0', 0.1)
+trillions_wfst.add_epsilon_transition(6, 7, '0', 0.1)
+trillions_wfst.add_epsilon_transition(7, 8, '0', 0.1)
+trillions_wfst.add_epsilon_transition(8, 9, '0', 0.1)
+trillions_wfst.add_epsilon_transition(9, 10, '0', 0.1)
+trillions_wfst.add_epsilon_transition(10, 11, '0', 0.1)
+trillions_wfst.add_epsilon_transition(11, 12, '0', 0.1)
+
+# Composite WFST
 composite_wfst = CompositeWFST()
 composite_wfst.add_wfst('units', units_wfst)
 composite_wfst.add_wfst('tens', tens_wfst)
 composite_wfst.add_wfst('hundreds', hundreds_wfst)
+composite_wfst.add_wfst('thousands', thousands_wfst)
+composite_wfst.add_wfst('millions', millions_wfst)
+composite_wfst.add_wfst('billions', billions_wfst)
+composite_wfst.add_wfst('trillions', trillions_wfst)
 
-input_sequence1 = ['twenty', 'two']
-result1 = composite_wfst.process(input_sequence1)
+import nltk
+from nltk import word_tokenize, pos_tag
+from typing import List, Tuple
 
-#print(result1[1])
+# Download necessary NLTK data (run this once)
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+
+def tokenize_and_tag(sentence: str) -> List[Tuple[str, str]]:
+    tokens = word_tokenize(sentence.lower())
+    return pos_tag(tokens)
+
+def identify_number_phrases(tagged_tokens: List[Tuple[str, str]]) -> List[Tuple[int, int]]:
+    number_words = set([
+        'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+        'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen',
+        'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety',
+        'hundred', 'thousand', 'million', 'billion', 'trillion'
+    ])
+    
+    phrases = []
+    start = -1
+    for i, (token, pos) in enumerate(tagged_tokens):
+        if token in number_words or pos == 'CD':  # CD is the POS tag for cardinal numbers
+            if start == -1:
+                start = i
+        elif start != -1:
+            phrases.append((start, i))
+            start = -1
+    
+    if start != -1:
+        phrases.append((start, len(tagged_tokens)))
+    
+    return phrases
+
+def process_sentence(sentence: str, composite_wfst: CompositeWFST) -> str:
+    tagged_tokens = tokenize_and_tag(sentence)
+    number_phrases = identify_number_phrases(tagged_tokens)
+    
+    tokens = [token for token, _ in tagged_tokens]
+    
+    for start, end in reversed(number_phrases):
+        number_words = tokens[start:end]
+        normalized = composite_wfst.process(number_words)
+        if normalized:
+            tokens[start:end] = [normalized[1]]  # Replace with normalized number
+    
+    return ' '.join(tokens)
+
+# Example usage
+sentence = "The shoes cost three hundred dollars and fifty cents"
+result = process_sentence(sentence, composite_wfst)
+print(result)
